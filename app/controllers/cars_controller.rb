d802288b88506d50d23236bc50b6e4beec549cc7
Dominic Tremblay <dominictremblay24@gmail.com>
2019-04-23 11:17:26 -0400

    demo march 4

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,13 +1,11 @@
 class CarsController < ApplicationController
   def index
     @cars = Car.all
-    @cars = @cars.where(make: params[:make]) if params[:make].present? 
-    @cars = @cars.where(style: params[:style]) if params[:style].present? 
-    @cars = @cars.where(model: params[:model]) if params[:model].present? 
-    @cars = @cars.where(trim: params[:trim]) if params[:trim].present? 
-    @cars = @cars.where(year: params[:year]) if params[:year].present? 
-    
+
+    @cars = @cars.where(make: params[:make]) if params[:make].present?
+    @cars = @cars.where(model: params[:model]) if params[:model].present?
+    @cars = @cars.where(style: params[:style]) if params[:style].present?
+    @cars = @cars.where(trim: params[:trim]) if params[:trim].present?
+    @cars = @cars.where(year: params[:year]) if params[:year].present?
   end
-
-
 end
--- spec/features/cars_spec.rb
+++ spec/features/cars_spec.rb
@@ -1,63 +1,78 @@
 require 'rails_helper'
 require 'support/database_cleaner'
 
-RSpec.feature "Cars", type: :feature, js: true do
+RSpec.feature "Cars", type: :feature, js:true do
+
 
   before :each do
-    @car1 = Car.create!({
+
+    @car1 = Car.create({
       make: Make.new(make: 'Lincoln'),
       model: Model.new(model: 'M3'),
-      trim: Trim.new(trim_level: 'XLE'),
       style: Style.new(body_style: 'Extended Cab Pickup'),
+      trim: Trim.new(trim_level: 'XLE'),
       year: 1971,
       colour: 'Red'
     })
 
-    @car2 = Car.create!({
+    @car2 = Car.create({
       make: Make.new(make: 'Buick'),
       model: Model.new(model: 'Mustang'),
-      trim: Trim.new(trim_level: 'XLE'),
       style: Style.new(body_style: 'Passenger Van'),
-      year: 2002,
-      colour: 'Mint green'
-    })
-
-    @car3 = Car.create!({
-      make: Make.new(make: 'Tesla'),
-      model: Model.new(model: 'T'),
       trim: Trim.new(trim_level: 'XLE'),
-      style: Style.new(body_style: 'Sedan coupe'),
-      year: 2019,
-      colour: 'Olive'
+      year: 2001,
+      colour: 'Indigo'
     })
 
+    @car3 = Car.create({
+      make: Make.new(make: 'Ford'),
+      model: Model.new(model: 'Escape'),
+      style: Style.new(body_style: 'Two Door Coupe'),
+      trim: Trim.new(trim_level: 'SAW'),
+      year: 2019,
+      colour: 'Midnight Blue'
+    })
+
+
   end
+
+
+  # scenario 'Display the list of cars' do
   
-  scenario 'List all the cars' do
+  #   visit '/cars'
+
+  #   expect(page).to have_css('.car', count: 3)
+  #   expect(page).to have_text('Lincoln M3 Extended Cab Pickup XLE 1971')
+  #   expect(page).to have_text('Buick Mustang Passenger Van XLE 2001')
+  #   expect(page).to have_text('Ford Escape Two Door Coupe SAW 2019')
+
+
+  #   save_screenshot 'all_cars.png'
+
+  # end
+
+
+  scenario 'filter according to the make' do
 
     visit '/cars'
 
-    expect(page).to have_text('Lincoln M3 Extended Cab Pickup XLE 1971', count: 1)
-    expect(page).to have_text('Buick Mustang Passenger Van XLE 2002', count: 1)
-    expect(page).to have_text('Tesla T Sedan coupe XLE 2019', count: 1)
-    save_screenshot('all_cars.png')
-  end
+    # select Ford from the make list
 
-  scenario 'Filter cars by make' do
-    
-    visit '/cars'
-    save_screenshot('before.png')
+    save_screenshot("before_filter.png")
 
-    expect(page).to have_text('All My Cars!')
-
-    # find('#make').set('Tesla')
-    within 'form' do
-      select 'Tesla', from: 'make'  
+    within '.search-form' do
+      select 'Ford', from: 'make'
       click_button 'Search!'
     end
-    expect(page).to have_text('Tesla T Sedan coupe XLE 2019', count: 1)
-    expect(page).to_not have_text('Lincoln M3 Extended Cab Pickup XLE 1971')
-    save_screenshot('filter_make.png')
+
+    expect(page).to have_css('.car', count: 1)
+    expect(page).to have_text('Ford Escape Two Door Coupe SAW 2019')
+
+    save_screenshot 'filter_make.png'
+
   end
+
+
+
 
 end
